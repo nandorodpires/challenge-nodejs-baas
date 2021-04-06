239751a73fc3ca7767d949916ffd77c3da292061
Fernando Rodrigues <nandorodpires@gmail.com>
2021-04-06 10:53:21 -0300

    feat: Person Controller

---
+++ src/app/controllers/personsController.js
@@ -0,0 +1,5 @@
+exports.index = () => ({ message: "List of persons" });
+exports.create = () => ({ message: "New person" });
+exports.show = () => ({ message: "Show person detail" });
+exports.update = () => ({ message: "Update person" });
+exports.delete = () => ({ message: "Delete person" });
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 
+const personsController = require("./app/controllers/personsController");
+
 const router = express.Router();
 
 // home
 router.get("/", (req, res) => {
   res.json({ message: "API is running..." });
 });
+
+// Persons
+router.get("/persons", (req, res) => res.json(personsController.index()));
+router.get("/persons/:id", (req, res) => res.json(personsController.show()));
+router.post("/persons", (req, res) => res.json(personsController.create()));
+router.put("/persons/:id", (req, res) => res.json(personsController.update()));
+router.delete("/persons/:id", (req, res) =>
+  res.json(personsController.delete())
+);
 
 module.exports = router;
